1b6aac76a2fe902e8adf5e74d52c8e21f456aeb3
DetectiveJAVA <ken@douglasland.com>
2023-12-17 14:12:33 -0800

    Added playerInterface for player datatype

---
+++ textrpg/playerInterface.java
@@ -0,0 +1,53 @@
+package textrpg;
+/**
+ * Interface for the player
+ * @author Ken Douglas
+ */
+public interface playerInterface {
+
+    //Starting stats
+    public final int DEFAULT_HEALTH = 100;
+    public final int DEFAULT_MAGIC = 50;
+    public final int DEFAULT_STAMINA = 50;
+    public final int DEFAULT_LEVEL = 1;
+    public final int DEFAULT_MORALITY = 50;
+    //Add data types of spells, weapons, and items. Put them in arrays.
+
+    //Methods for the player to various actions from attacking to crafting
+
+    /**
+     * Getter methods for player's stats
+     */
+     public int getHP();
+     public int getMagic();
+     public int getStamina();
+     public int getLevel();
+     public int getMorality();
+
+    /**
+    * Lose X from the player's pools of stats.
+    */
+    public void loseHP(int lose);
+    public void loseMagic(int lose);
+    public void loseStamina(int lose);
+    public void loseMorality(int lose);
+
+    /**
+    * Gain X towards the player's pool of stats.
+    */
+    public void gainHP(int gain);
+    public void gainMagic(int gain);
+    public void gainStamina(int gain);
+    public void gainMorality(int gain);
+
+    /**
+    * Level up method for when the player gains enough XP
+    */
+    public void levelUp();
+
+
+
+
+
+
+}
---
+++ textrpg/textRPGDriver.java
@@ -0,0 +1,11 @@
+package textrpg;
+public class textRPGDriver {
+    public static void main(String[] args){
+        /*
+        This is where the main dirver method will make calls to the RPG methods who will
+        pull all the data from the data structures.
+        */
+        
+    
+    }
+}
